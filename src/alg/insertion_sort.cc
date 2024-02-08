f7ce70b20a092a8d34a0d57d014ec2bcef119ec5
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-02-08 20:10:51 +0100

    Add insertion sort algorithm

---
+++ src/alg/insertion_sort.cc
@@ -0,0 +1,33 @@
+#include <cstddef>
+#include <gtest/gtest.h>
+#include <utility>
+#include <vector>
+
+namespace alg {
+void InsertionSort(std::vector<int> &elements) {
+  std::size_t i{1};
+
+  while (i < elements.size()) {
+    std::size_t j{i};
+
+    while (j > 0 && elements.at(j - 1) > elements.at(j)) {
+      std::swap(elements.at(j), elements.at(j - 1));
+      --j;
+    }
+
+    ++i;
+  }
+}
+} // namespace alg
+
+namespace {
+using alg::InsertionSort;
+
+TEST(InsertionSortTest, SortElementsInAscendingOrder) {
+  std::vector<int> elements{16, 4, 32, 2, 1, 128, 8, 64};
+  InsertionSort(elements);
+
+  std::vector<int> expected{1, 2, 4, 8, 16, 32, 64, 128};
+  EXPECT_EQ(elements, expected);
+}
+} // namespace
