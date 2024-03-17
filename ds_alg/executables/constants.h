8a7cffc9003a1c7c365b6d6034aeb1329a189116
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-03-17 18:02:07 +0100

    Format code
    Extract shared code to new files.

---
+++ ds_alg/executables/constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace {
+constexpr int kScreenWidth{300};
+constexpr int kScreenHeight{300};
+constexpr int kNumberOfBars{50};
+}  // namespace
--- ds_alg/executables/insertion_sort_visualizer.cc
+++ ds_alg/executables/insertion_sort_visualizer.cc
@@ -1,33 +1,12 @@
 #include <algorithm>
 #include <chrono>
-#include <iostream>
-#include <iterator>
-#include <optional>
-#include <random>
 #include <raylib-cpp.hpp>
 #include <thread>
 #include <utility>
 #include <vector>
 
-namespace {
-constexpr int kScreenWidth{300};
-constexpr int kScreenHeight{300};
-constexpr int kNumberOfBars{50};
-}  // namespace
-
-void DrawBars(const std::vector<int> &bars,
-              std::optional<int> selected_bar = std::nullopt) {
-  int width{kScreenWidth / kNumberOfBars};
-  for (auto i{0}; i < kNumberOfBars; ++i) {
-    if (selected_bar.has_value() && selected_bar.value() == i) {
-      DrawRectangle(i * width, kScreenWidth - bars.at(i), width, bars.at(i),
-                    BLUE);
-    } else {
-      DrawRectangle(i * width, kScreenWidth - bars.at(i), width, bars.at(i),
-                    WHITE);
-    }
-  }
-}
+#include "constants.h"
+#include "utilities.h"
 
 void InsertionSortVisualization(std::vector<int> &bars) {
   std::size_t i{1};
@@ -53,20 +32,11 @@ void InsertionSortVisualization(std::vector<int> &bars) {
 }
 
 int main() {
-  std::random_device random_device{};
-  std::mt19937 rng(random_device());
-  std::uniform_int_distribution<std::mt19937::result_type> distribution(
-      1, kScreenHeight / kNumberOfBars);
-
-  std::vector<int> bars(kNumberOfBars);
-  for (auto i{0}; i < kNumberOfBars; ++i) {
-    bars.at(i) = (i + distribution(rng)) * distribution(rng);
-  }
-  std::shuffle(std::begin(bars), std::end(bars), rng);
-
   raylib::Window window(kScreenWidth, kScreenHeight,
                         "Insertion Sort Visualization");
   window.SetTargetFPS(60);
+
+  auto bars{CreateBars()};
 
   while (!window.ShouldClose()) {
     BeginDrawing();
